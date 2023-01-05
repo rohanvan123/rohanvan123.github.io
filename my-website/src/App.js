a823933640f2fba79795979dfe86fa8c90b88978
Rohan Vanjani <67704979+rohanvan123@users.noreply.github.com>
2023-01-05 16:09:06 -0600

    added personal bio

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,18 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+        className="container" 
+        style={{width: "450px"}}
         >
-          Learn React
-        </a>
+          <h1 
+          className="typed-out">
+            Rohan Vanjani
+          </h1>
+        </div>
+        <div 
+        style ={{width: '700px', align: "left", fontSize: "22px"}} >
+          <p>
+            Hello! My name is Rohan Vanjani, and I am a sophomore at the <em className="sp-text">University of Illinois at Urbana-Champaign </em>
+            currently pursuing a <i>B.S.</i> in <em className="sp-text">Computer Science & Mathematics</em>. I have a passion for programming 
+            and building robust full-stack applications. I am currently seeking opportunities related to <em className="sp-text">Software Engineering</em>, 
+            Web Development, and Product Development. On campus, I am currently <em className="sp-text">Software Developer</em> for <a className="sp-text" href="https://uiuc.hack4impact.org/">Hack4Impact</a>,
+            where we build full-stack applications for local nonprofits. Additionally, I served as the past <em className="sp-text">Technology Chair</em> for <em className="sp-text">Alpha Kappa Psi</em>, a professional business fraternity. 
+            Outside of school, I am an avid tutor and also enjoy weightlifting, investing, and watching basketball. 
+          </p>
+        </div>
       </header>
     </div>
   );
